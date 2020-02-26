2201da3d2f915ec46d6d0366aad0fc5a786f9058
Takashi Saito <takashi11171117@gmail.com>
2020-02-26 18:20:34 +0900

    connected socket.io

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const PORT = 3000;
 
 app.get('/', function(req, res) {
   res.render('index.ejs');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const chat = io.on('connection', function (client) {
+  console.log('connected');
+  client.emit('connected', 'aaaaaa');
+});
+
+// app.io.route('ready', function(req) {
+//   req.io.join(req.data)
+//   app.io.room(req.data).broadcast('announce', {
+//     message: `New client in the ${req.data} room.`
+//   })
+// })
+
+server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
